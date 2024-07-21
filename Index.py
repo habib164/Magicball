c7b6ed1f4c57ad120ca8b8a5a5b78bacbab37a22
Said Alfahulmizan <81120010+habib164@users.noreply.github.com>
2024-07-21 21:54:14 +0700

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,35 @@
+import random
+
+name = input("")
+
+question = input("What's your question? ")
+
+answer = ""
+
+random_num = random.randint(1, 9)
+
+#print(random_num)
+
+if random_num == 1:
+  answer = "Yes - definitely"
+elif random_num == 2:
+  answer = "It is decidedly so"
+elif random_num == 3:
+  answer = "Without a doubt"
+elif random_num == 4:
+  answer = "Reply hazy, try again"
+elif random_num == 5:
+  answer = "Ask again later"
+elif random_num == 6:
+  answer = "Better not tell you now"
+elif random_num == 7:
+  answer = "My sources say no"
+elif random_num == 8:
+  answer = "Outlook not so good"
+elif random_num == 9:
+  answer = "Very doubtful"
+else:
+  answer = "Error"
+
+print(name, "asks:", question)
+print("Magic ball's answers:", answer)
